66c558bef562fae637413573899b61085f30ac75
Simeon Jackman <simeon@jackman.ch>
2024-10-11 16:41:30 +0200

    ADD: some style

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-count = 0
+let count = 0
+let disp = document.getElementById("display");
 
 function myFunction(){
     console.log("hello world")
     console.log("clicked " + count + " times...")
     count ++;
+    disp.innerHTML = count;
 }
